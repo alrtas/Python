4ceda803316dcb4b3e0343cc3b3ade8c7f45165b
Thiago <alrrtas@gmail.com>
2019-07-16 23:49:58 -0300

    creat maximum_number.py

---
+++ maximum_number.py
@@ -0,0 +1,11 @@
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+
+if a>b:
+    print(str(a)+" is bigger than "+str(b))
+elif b>a:
+    print(str(b)+" is bigger than "+str(a))
+else:
+    print(str(a) + " and " + str(b)+" has the same size")
+
+
