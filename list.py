e722790aac199aa1f3e12a1b3de9bec9b799ef94
Thiago <alrrtas@gmail.com>
2019-07-19 19:02:06 -0300

    creat odd_even.py
    Write a function called showNumbers that takes a parameter called limit. It should print all the numbers between 0 and limit with a label to identify the even and odd numbers. For example, if the limit is 3, it should print:
    0 EVEN
    1 ODD
    2 EVEN
    3 ODD

---
+++ list.py
@@ -0,0 +1,40 @@
+
+
+bikeList=['ducati', 'yamaha', 'honda', 'suzuki']
+print(bikeList[1])
+
+bikeList.append('kawasaki')
+bikeList.remove(bikeList[1])
+print(bikeList[1])
+
+bikeList.remove('suzuki')
+print(bikeList[-1])
+
+bikeList.append('yamaha')
+bikeList.append('suzuki')
+bikeList.append('aprila')
+bikeList.append('bmw')
+
+bikeList.sort()
+print(bikeList)
+
+bikeList.sort(reverse=True)
+print(bikeList)
+print(len(bikeList))
+
+print("\n Novo exercicio \n")
+
+
+places = ['new york', 'monaco', 'amsterda', 'rome', 'tokyo']
+print(places)
+print(places.sort())
+print(places)
+print(places.sort(reverse=True))
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+print(len(places))
