841db2abce00a75ac8955f25088658e082ec2f30
Thiago <alrrtas@gmail.com>
2019-07-23 20:45:03 -0300

    not complete

---
+++ multiples.py
@@ -0,0 +1,14 @@
+def funcSumMultiples(number):
+
+    listMulti['1']
+
+    if number % 3 == 0:
+        n3 = number/3
+
+
+    elif number % 5 == 0:
+
+    else:
+        "Number is not a multiple of 3 or 5"
+
+randomNumber = int(input("Enter a number: "))
