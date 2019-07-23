2e14dc3d7b4a0175d81d3415bd26876e3a13df0e
Thiago <alrrtas@gmail.com>
2019-07-22 21:30:21 -0300

    Creat dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+print("\n ---------- New Exercises ---------- \n ")
+
+#Creating a dictionary
+alien_0 = {'color': 'green', 'points': '5'}
+
+print("Aliens color is: " + alien_0['color'])
+print(alien_0)
+
+#Adding a value on a dictionary
+alien_0['x_position'] = '0'
+alien_0['y_position'] = '25'
+print(alien_0)
+
+
+alien_1 = {}
+alien_1['color:'] = 'blue'
+alien_1['points'] = '15'
+alien_1['x_position'] = '10'
+alien_1['y_position'] = '35'
