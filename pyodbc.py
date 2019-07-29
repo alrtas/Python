dcb30a65acb6c57d587c7c4544f0f9b005b7a1d5
Thiago <alrrtas@gmail.com>
2019-07-28 23:52:36 -0300

    connecting with mysql

--- pyodbc.py
+++ pyodbc.py
@@ -1,8 +1,12 @@
-import pyodbc
 
-conn = pyodbc.connect(
-    "Driver = {SQL Server Nativa Client 11.0};"
-    "Server = localhost;"
-    "Data = ;"
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="12345678",
 
 )
+
+print(mydb)
