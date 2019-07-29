89882f066d79cd31a87079d7a99f0615d6f0428b
Thiago <alrrtas@gmail.com>
2019-07-29 00:18:27 -0300

    Appliyng some filters in database with select *

--- pyodbc.py
+++ pyodbc.py
@@ -12,7 +12,7 @@ mydb = mysql.connector.connect(
 
 
 mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM table_client_old ")
+mycursor.execute("SELECT telefone FROM table_client_old ")
 
 myresult = mycursor.fetchall()
 
@@ -20,3 +20,30 @@ for row in myresult:
     print(row)
 
 #mostra todos os dados da tabela
+
+
+sql = "SELECT * FROM table_client_old WHERE telefone = 8409829376.0"
+
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for result in myresult:
+    print(result)
+
+#search for a specific number, then brings all the data from this row
+
+
+sql1 = "SELECT * FROM table_client_old  WHERE nome  LIKE 'Mi%'"
+mycursor.execute(sql1)
+myresult = mycursor.fetchall()
+for result1 in myresult:
+    print(result1)
+
+
+
+sql2 = "SELECT * FROM table_client_old WHERE nome = %s"
+
+mycursor.execute(sql2,("Thiago",))
+myresult = mycursor.fetchall()
+for result2 in myresult:
+    print(result2)
+
