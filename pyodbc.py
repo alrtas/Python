12b1f499d1fcc36aaf3f8c854206a5c0d2434721
Thiago <alrrtas@gmail.com>
2019-07-29 00:04:07 -0300

    show all data from table

--- pyodbc.py
+++ pyodbc.py
@@ -6,7 +6,17 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="12345678",
+    database="client",
 
 )
 
-print(mydb)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM table_client_old ")
+
+myresult = mycursor.fetchall()
+
+for row in myresult:
+    print(row)
+
+#mostra todos os dados da tabela
